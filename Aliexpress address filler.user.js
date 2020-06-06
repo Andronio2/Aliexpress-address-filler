92e6c2b80a437421fd521f214ec832b5a318d695
Andronio <gprsboost03@mail.ru>
2020-06-06 13:12:27 +0600

    Добавил получение всех данных из строки

--- Aliexpress address filler.user.js
+++ Aliexpress address filler.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Aliexpress address filler
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  try to take over the world!
+// @version      1.1
+// @description  Заполняет поля адресных данных на Алиэкспресс
 // @author       Andronio
 // @homepage     https://github.com/Andronio2/Aliexpress-address-filler
 // @supportURL   https://github.com/Andronio2/Aliexpress-address-filler/issues
@@ -24,132 +24,138 @@
 будет 0, 1, 2
 */
 
-let myIndex     = "150013";
+let myIndex     = "100000";
 let myPhoneCode = "+7";
 let myCountry   = "KZ";
-let myProvince  = "Severo-Kazakhstanskaya oblast";
-let myCity      = "Petropavlovsk";
+let myProvince  = "oblast";
+let myCity      = "gorod";
 
 let orderPhone  = 0;
 let orderAddr   = 1;
 let orderName   = 2;
+let orderCountry= 3;
+let orderCity   = 4;
+let orderProvince = 5;
+let orderIndex  = 6;
+let orderPhCode = 7;
 
 /*
  * Дальше не трогать
  */
     let div = document.createElement('div');
-    div.className = 'main-box';
+    div.className = 'main-box-address';
 
     div.innerHTML += `
-		<input type="text" id="namepass"></br>
-		<button type="button" id="fillbtn" class="fill-btn">Заполнить</button>
-		`;
+        <input type="text" id="namepassaddr"></br>
+        <button type="button" id="fillbtnaddr" class="fill-btn-address">Заполнить</button>
+        `;
 
 // Стили
     let styles = `
-    .main-box {
-		position: fixed;
-		top: 0;
-		right: 0;
-		background: white;
-		box-shadow: 1px -1px 4px 1px;
-		max-width: 40%;
-		max-height: 400px;
-		padding: 10px 20px;
-		overflow-y: hidden;
-		overflow-x: hidden;
-		z-index:9999;
+    .main-box-address {
+        position: fixed;
+        top: 0;
+        right: 0;
+        background: white;
+        box-shadow: 1px -1px 4px 1px;
+        max-width: 40%;
+        max-height: 400px;
+        padding: 10px 20px;
+        overflow-y: hidden;
+        overflow-x: hidden;
+        z-index:9999;
     }
 
-    .fill-btn {
-		display: inline;
-		padding: 5px 10px;
-		margin-right:auto;
-		cursor:pointer;
+    .fill-btn-address {
+        display: inline;
+        padding: 5px 10px;
+        margin-right:auto;
+        cursor:pointer;
     }`;
 
-	let styleSheet = document.createElement('style');
-	styleSheet.type = "text/css";
-	styleSheet.innerText = styles;
-	document.head.append(styleSheet);
-	document.body.append(div);
-    let fillBtn = document.getElementById('fillbtn');
+    let styleSheet = document.createElement('style');
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+    document.head.append(styleSheet);
+    document.body.append(div);
+    let fillBtn = document.getElementById('fillbtnaddr');
     fillBtn.addEventListener('click', fillAddrFunc);
-    let textInput = document.getElementById('namepass');
+    let textInput = document.getElementById('namepassaddr');
     textInput.addEventListener('keydown', event => {
         if (event.keyCode == "13") {
-            document.getElementById('fillbtn').click();
+            document.getElementById('fillbtnaddr').click();
         }
     });
     textInput.focus();
 
     let currMode = 0;
-    let oldCountry = "";
+    let oldProvince = "";
 
-	// Функция заполения адреса
-	function fillAddrFunc() {
-		let elemCountry = document.querySelector('select[name="country"]');
-		let elemProvinceSelect = document.querySelector('.sa-province-wrapper > select');
-		let elemProvinceInput = document.querySelector('.sa-province-wrapper > input');
-		let elemCitySelect = document.querySelector('.sa-city-wrapper > select');
-		let elemCityInput = document.querySelector('.sa-city-wrapper > input');
+    // Функция заполения адреса
+    function fillAddrFunc() {
+        let elemCountry = document.querySelector('select[name="country"]');
+        let elemProvinceSelect = document.querySelector('.sa-province-wrapper > select');
+        let elemProvinceInput = document.querySelector('.sa-province-wrapper > input');
+        let elemCitySelect = document.querySelector('.sa-city-wrapper > select');
+        let elemCityInput = document.querySelector('.sa-city-wrapper > input');
+        let namepass = document.getElementById('namepassaddr');
+        let mass;
 
-		switch(currMode) {
-			case 0:
-				let namepass = document.getElementById("namepass");
-				let mass;
+        if (namepass.value == "") return alert("Нет данных");
+        if (/.+\t.+\t.+/.test(namepass.value)) {
+            mass = namepass.value.split('\t');
+        } else if (/.+:.+:.+/.test(namepass.value)) {
+            mass = namepass.value.split(':');
+        } else {
+            namepass.value = "";
+            return alert("Неправильный формат");
+        }
+        let tempCountry = myCountry ? myCountry : mass[orderCountry];
 
-				if (namepass.value == "") return alert("Нет данных");
-				if (/.+\t.+\t.+/.test(namepass.value)) {
-					mass = namepass.value.split('\t');
-				} else if (/.+:.+:.+/.test(namepass.value)) {
-					mass = namepass.value.split(':');
-				} else {
-					namepass.value = "";
-					return alert("Неправильный формат");
-				}
+        switch(currMode) {
+            case 0:
 
-				document.querySelector('input[name="contactPerson"]').value = mass[orderName];
-				document.querySelector('input[name="address"]').value = mass[orderAddr];
-				document.querySelector('input[name="zip"]').value = myIndex;
-				document.querySelector('input[name="mobileNo"]').value = mass[orderPhone];
-				elemCountry.value = myCountry;
-				oldCountry = elemProvinceSelect.options[1].text;
-				elemCountry.dispatchEvent(new Event("change"));
-				currMode = 1;
-				return setTimeout(fillAddrFunc, 200);
-				break;
-			case 1:
-				if (elemProvinceSelect.options[1].text == oldCountry && myCountry != "BY")
-					return setTimeout(fillAddrFunc, 200);
-				if (myCountry == "BY") {
-					elemProvinceInput.value = myProvince;
-				} else {
-					elemProvinceSelect.value = myProvince;
-					elemProvinceSelect.dispatchEvent(new Event("change"));
-				}
-				currMode = 2;
-				return setTimeout(fillAddrFunc, 200);
-				break;
-			case 2:
-				if (elemCitySelect.length == 0 && myCountry != "BY")
-					return setTimeout(fillAddrFunc, 200);
-				if (myCountry == "BY") {
-					elemCityInput.value = myCity;
-				} else {
-					elemCitySelect.value = myCity;
-					elemCitySelect.dispatchEvent(new Event("change"));
-				}
-				currMode = 3;
-				return setTimeout(fillAddrFunc, 200);
-				break;
-			case 3:
-				document.querySelector('input[name="phoneCountry"]').value = myPhoneCode;
-				currMode = 0;
-				document.querySelector('.sa-confirm').click();
-				break;
-			default:
-				alert("Ошибка в программе");
-		}
-	}
+                document.querySelector('input[name="contactPerson"]').value = mass[orderName];
+                document.querySelector('input[name="address"]').value = mass[orderAddr];
+                document.querySelector('input[name="zip"]').value = myIndex ? myIndex : mass[orderIndex];
+                document.querySelector('input[name="mobileNo"]').value = mass[orderPhone];
+                oldProvince = elemProvinceSelect.options[1].text;
+                elemCountry.value = tempCountry;
+                elemCountry.dispatchEvent(new Event("change"));
+                currMode = 1;
+                return setTimeout(fillAddrFunc, 200);
+                break;
+            case 1:
+                if (elemProvinceSelect.options[1].text == oldProvince && tempCountry != "BY")
+                    return setTimeout(fillAddrFunc, 200);
+                if (tempCountry == "BY") {
+                    elemProvinceInput.value = myProvince ? myProvince : mass[orderProvince];
+                } else {
+                    elemProvinceSelect.value = myProvince ? myProvince : mass[orderProvince];
+                    elemProvinceSelect.dispatchEvent(new Event("change"));
+                }
+                currMode = 2;
+                return setTimeout(fillAddrFunc, 200);
+                break;
+            case 2:
+                if (elemCitySelect.length == 0 && tempCountry != "BY")
+                    return setTimeout(fillAddrFunc, 200);
+                if (tempCountry == "BY") {
+                    elemCityInput.value = myCity ? myCity : mass[orderCity];
+                } else {
+                    elemCitySelect.value = myCity ? myCity : mass[orderCity];
+                    elemCitySelect.dispatchEvent(new Event("change"));
+                }
+                currMode = 3;
+                return setTimeout(fillAddrFunc, 200);
+                break;
+            case 3:
+                document.querySelector('input[name="phoneCountry"]').value = myPhoneCode ? myPhoneCode : mass[orderPhCode];
+                currMode = 0;
+                document.querySelector('.sa-confirm').click();
+                break;
+            default:
+                alert("Ошибка в программе");
+        }
+    }
 })();
